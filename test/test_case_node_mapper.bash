fb44aceaeab3849880792282abb5a27e7f8b2a1b
James McCombs <jmccombs@iu.edu>
2025-05-21 12:43:17 -0400

    Debugging case_node_mapper

--- test/test_case_node_mapper.bash
+++ test/test_case_node_mapper.bash
@@ -1,16 +1,29 @@
 #!/usr/bin/env bash
 
+this_script_name=`basename $0`
 MAPPERS_HOME="$(cd "`dirname "$0"`"/..; pwd)"
 
 source ${MAPPERS_HOME}/test/test_config.bash
 
-version=$(${MAPPERS_HOME}/python/ardac --version)
-echo "version='${version}'"
+conda activate ${MAPPERS_HOME}/venv
+conda_activation_status=$?
 
-dcc_version=$(${MAPPERS_HOME}/python/ardac --dcc_version)
-echo "dcc_version='${dcc_version}'"
+if [[ $conda_activation_status -ne 0 ]]
+then
+   echo "ERROR($this_script_name): Failed to activate Python virtual environment" 1>&2
+   exit ${VIRTUAL_ENVIRONMENT_FAILURE}
+fi
 
-mapping_version=$(${MAPPERS_HOME}/python/ardac --mapping_version)
-echo "mapping_version='${mapping_version}'"
+
+mapper_script=${MAPPERS_HOME}/python/ardac/case_node_mapper.py
+
+version=$(python ${mapper_script} --version)
+echo "INFO($this_script_name): version='${version}'"
+
+dcc_version=$(python ${mapper_script} --dcc_version)
+echo "INFO($this_script_name): dcc_version='${dcc_version}'"
+
+mapping_version=$(python ${mapper_script} --mapping_version)
+echo "INFO($this_script_name): mapping_version='${mapping_version}'"
 
 
