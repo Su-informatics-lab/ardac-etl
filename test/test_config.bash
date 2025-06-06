7c5d5004d453a4cc31e126734dc2cc58d48b115e
James McCombs <jmccombs@iu.edu>
2025-06-06 11:31:46 -0400

    Changed templates directory name

--- test/test_config.bash
+++ test/test_config.bash
@@ -1,6 +1,6 @@
 VIRTUAL_ENVIRONMENT_FAILURE=1
 
-NODE_TEMPLATES_PATH=/N/project/ardac/development/dcc_v2.0.0_data/templates
+NODE_TEMPLATES_PATH=/N/project/ardac/development/dcc_v2.0.0_data/node_templates
 DCC_OBS_PATH=/N/project/ardac/development/dcc_v2.0.0_data/obs_data
 DCC_RCT_PATH=/N/project/ardac/development/dcc_v2.0.0_data/rct_data
 NODE_OUTPUT_PATH=/N/project/ardac/development/cdm_testing/node_output
