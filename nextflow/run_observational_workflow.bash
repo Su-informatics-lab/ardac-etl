9056c1fc291a5e81924bb4b0080c042da1619e22
James McCombs <jmccombs@iu.edu>
2025-06-19 18:08:29 -0400

    Updated README and NextFlow configuration file comments

--- nextflow/run_observational_workflow.bash
+++ nextflow/run_observational_workflow.bash
@@ -1,15 +1,3 @@
-#!/bin/bash
-#SBATCH -J test_workflow
-#SBATCH -p general
-#SBATCH -o %x_%j.txt
-#SBATCH -e %x_%j.err
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=jmccombs@iu.edu
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --time=05:00:00
-#SBATCH --mem=1G
-#SBATCH -A r00606
 
-nextflow run ardac_etl_workflow.nf -config nextflow.config -profile conda,workstation --subjects_type observational
+nextflow run ardac_etl_workflow.nf -config nextflow.config -profile conda,workstation -log-level DEBUG --subjects_type observational
 
